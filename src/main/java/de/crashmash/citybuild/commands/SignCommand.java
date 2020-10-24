db7ada0799d9a554602263a40fc4260e54c3c3a8
Damon Günther <info@damon-guenther.de>
2020-10-24 14:32:25 +0200

    created SignCommand editing ConfigData -> Sign function

---
+++ src/main/java/de/crashmash/citybuild/commands/SignCommand.java
@@ -0,0 +1,16 @@
+package de.crashmash.citybuild.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class SignCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+
+
+
+        return false;
+    }
+}
--- src/main/java/de/crashmash/citybuild/data/ConfigData.java
+++ src/main/java/de/crashmash/citybuild/data/ConfigData.java
@@ -6,5 +6,7 @@ public class ConfigData {
 
     public static boolean CONFIG_COMMAND_SCHILD = CityBuildV2.getPlugin().getConfig().getBoolean("Settings.Commands.Schild");
     public static boolean CONFIG_COMMAND_CHATCLEAR = CityBuildV2.getPlugin().getConfig().getBoolean("Settings.Commands.ChatClear");
+    public static boolean CONFIG_COMMAND_SIGN = CityBuildV2.getPlugin().getConfig().getBoolean("Settings.Commands.Sign");
+    public static boolean CONFIG_COMMAND_UNSIGN = CityBuildV2.getPlugin().getConfig().getBoolean("Settings.Commands.UnSign");
 
 }
