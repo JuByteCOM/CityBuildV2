c150f163074b39af0b4b9cbc9590e92c94d13f45
JustinSGD <pohlers01@gmail.com>
2024-02-14 13:14:07 +0100

    Fixed player join error Updatet DeveloperAPI Fixed /head cooldown

--- src/main/java/com/jubyte/citybuild/commands/HeadCommand.java
+++ src/main/java/com/jubyte/citybuild/commands/HeadCommand.java
@@ -16,52 +16,52 @@ import java.text.SimpleDateFormat;
 
 public class HeadCommand extends AbstractCommand {
 
-  public HeadCommand() {
-    super(
-        ConfigData.CONFIG_COMMAND_HEAD_NAME,
-        null,
-        "Give yourself decorative heads from other players.",
-        ConfigData.CONFIG_COMMAND_HEAD_ALIASES);
-  }
+    public HeadCommand() {
+        super(
+                ConfigData.CONFIG_COMMAND_HEAD_NAME,
+                null,
+                "Give yourself decorative heads from other players.",
+                ConfigData.CONFIG_COMMAND_HEAD_ALIASES);
+    }
 
-  @Override
-  public boolean onCommand(
-      CommandSender commandSender, Command command, String s, String[] strings) {
-    if (!(commandSender instanceof Player)) {
-      commandSender.sendMessage(MessagesData.ISNOT_PLAYER);
-    } else {
-      Player player = (Player) commandSender;
-      if (player.hasPermission(MessagesData.HEAD_COMMAND_PERMISSION_USE)) {
-        if (strings.length == 1) {
-          CooldownPlayer cooldownPlayer =
-              CityBuildV2.getPlugin().getCooldownCache().getPlayerByUUID(player.getUniqueId());
-          if (!cooldownPlayer.hasHeadCooldown(player)) {
+    @Override
+    public boolean onCommand(
+            CommandSender commandSender, Command command, String s, String[] strings) {
+        if (!(commandSender instanceof Player)) {
+            commandSender.sendMessage(MessagesData.ISNOT_PLAYER);
+        } else {
+            Player player = (Player) commandSender;
+            if (!player.hasPermission(MessagesData.HEAD_COMMAND_PERMISSION_USE)) {
+                player.sendMessage(MessagesData.NOPERMS);
+                return false;
+            }
+            if (strings.length != 1) {
+                player.sendMessage(MessagesData.HEAD_COMMAND_MESSAGE_USAGE);
+                return false;
+            }
+            CooldownPlayer cooldownPlayer =
+                    CityBuildV2.getPlugin().getCooldownCache().getPlayerByUUID(player.getUniqueId());
+            if (cooldownPlayer.hasHeadCooldown(player)) {
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+                SimpleDateFormat simpleTimeFormat = new SimpleDateFormat("HH:mm:ss");
+                long time = cooldownPlayer.getHead();
+                player.sendMessage(
+                        MessagesData.HEAD_COMMAND_MESSAGE_COOLDOWN
+                                .replace("[date]", simpleDateFormat.format(time))
+                                .replace("[time]", simpleTimeFormat.format(time)));
+                return false;
+            }
             ItemStack itemStack = new ItemStack(Material.PLAYER_HEAD, 1);
             SkullMeta skullMeta = (SkullMeta) itemStack.getItemMeta();
             skullMeta.setOwner(strings[0]);
             skullMeta.setDisplayName(
-                MessagesData.HEAD_COMMAND_ITEM_DISPLAYNAME.replace("[skullName]", strings[0]));
+                    MessagesData.HEAD_COMMAND_ITEM_DISPLAYNAME.replace("[skullName]", strings[0]));
             itemStack.setItemMeta(skullMeta);
             player.getInventory().addItem(itemStack);
             player.sendMessage(
-                MessagesData.HEAD_COMMAND_MESSAGE_ADDED_HEAD.replace("[skullName]", strings[0]));
+                    MessagesData.HEAD_COMMAND_MESSAGE_ADDED_HEAD.replace("[skullName]", strings[0]));
             cooldownPlayer.setHead();
-          } else {
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
-            SimpleDateFormat simpleTimeFormat = new SimpleDateFormat("HH:mm:ss");
-            long time = cooldownPlayer.getHead();
-            player.sendMessage(
-                MessagesData.HEAD_COMMAND_MESSAGE_COOLDOWN
-                    .replace("[date]", simpleDateFormat.format(time))
-                    .replace("[time]", simpleTimeFormat.format(time)));
-          }
-        } else {
-          player.sendMessage(MessagesData.HEAD_COMMAND_MESSAGE_USAGE);
         }
-      } else {
-        player.sendMessage(MessagesData.NOPERMS);
-      }
+        return false;
     }
-    return false;
-  }
 }
